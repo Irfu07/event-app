ac2352706b5c32c09541dfb75927f043a6793f2b
IRFAN HUSSAIN <irfu1605@gmail.com>
2026-03-04 21:14:30 +0530

    backend fixed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const multer = require("multer");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 /* ================= DATABASE ================= */
+console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
